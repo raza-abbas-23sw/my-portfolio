e55cbf5afaa41f6d829f19c45a305806a8bbbd71
Raza Abbas <157038022+raza-abbas-23sw@users.noreply.github.com>
2025-05-25 23:10:54 +0500

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,6 @@ const About = () => {
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
     >
       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
-        {/* Left Side */}
         <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
           {/* Greeting */}
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
